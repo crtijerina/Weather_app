c3b686a69351489a9b1938bf00b7eec4f11e4724
Chris Tijerina <crtijerina1988@gmail.com>
2021-08-28 13:01:32 -0500

    added cards, and adding js

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+
+
+function getCityInfo(cityName){
+    var apiKey = "e3d698c2298d9ba4bb8bb9f4bda0cb45"
+    var currentDayURL="api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid="+apiKey
+}
+})  
